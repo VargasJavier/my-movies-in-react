eff4035723ad53d93648521462e5949f421b5698
Javier Vargas <jabo94454@gmail.com>
2022-06-27 12:39:20 -0500

    Se conectó a la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,59 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ContainerPages from "./Components/ContainerPages";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const URL_IMAGE = "https://image.tmdb.org/t/p/w500";
+  const [movies, setMovies] = useState([]);
+  const [initial, setInitial] = useState(0);
+  const [final, setFinal] = useState(5);
+  const [page, setPage] = useState(1);
+  const [disable, setDisable] = useState({ next: false, previous: true });
+  const URL =
+    "https://api.themoviedb.org/3/trending/all/day?api_key=a9623aebd3d503d051631e3bf4c4ef8f";
+
+  const getMovies = async () => {
+    const data = await fetch(URL);
+    const moviesData = await data.json();
+    setMovies(moviesData.results);
+  };
+
+  function changePage(evt, number) {
+    setPage(page + number);
+    isDisable(number);
+    setInitial(initial + number * 5);
+    setFinal(final + number * 5);
+  }
+
+  function isDisable(number) {
+    if (page + number < 2) setDisable({ ...disable, previous: true });
+    else if (page + number > 3) setDisable({ ...disable, next: true });
+    else setDisable({ next: false, previous: false });
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <h1>Películas</h1>
+      <ContainerPages changePage={changePage} disable={disable} page={page} />
+      {movies ? (
+        movies.slice(initial, final).map((m) => {
+          return (
+            <div key={m.id}>
+              <img src={URL_IMAGE + m.backdrop_path} />
+              <p>{m.name ? `${m.name}` : `${m.title}`}</p>
+            </div>
+          );
+        })
+      ) : (
+        <p>No se encontraron resultados</p>
+      )}
+      <ContainerPages changePage={changePage} disable={disable} page={page} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/ContainerPages.jsx
@@ -0,0 +1,14 @@
+const ContainerPages = ({ changePage, disable, page }) => {
+  return (
+    <div className='containerPages'>
+      <button onClick={(e) => changePage(e, -1)} disabled={disable.previous}>
+        Prev
+      </button>
+      <p>{`Page ${page}`}</p>
+      <button onClick={(e) => changePage(e, +1)} disabled={disable.next}>
+        Next
+      </button>
+    </div>
+  );
+};
+export default ContainerPages;
